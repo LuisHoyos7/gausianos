ac996d03c7f0bd6292fd24dbfce61c162cee38b5
luis <luishoyosolaya.com>
2020-06-11 10:05:46 -0500

    integracion plantilla metronic

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+	<!--begin::Head-->
+		@include("layouts.head")
+	<!--end::Head-->
+
+	<!--begin::Body-->
+	<body id="kt_body" class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed aside-minimize-hoverable page-loading">
+		<!--begin::Main-->
+		<div class="d-flex flex-column flex-root">
+			<!--begin::Login-->
+			<div class="login login-3 login-signin-on d-flex flex-row-fluid" id="kt_login">
+				<div class="d-flex flex-center bgi-size-cover bgi-no-repeat flex-row-fluid" style="background-image: url(metronic/dist/assets/media/bg/bg-1.jpg);">
+					<div class="login-form text-center text-white p-7 position-relative overflow-hidden">
+						<!--begin::Login Header-->
+						<div class="d-flex flex-center mb-15">
+							<a href="#">
+								<img src="assets/media/logos/logo-letter-9.png" class="max-h-100px" alt="" />
+							</a>
+						</div>
+						<!--end::Login Header-->
+						<!--begin::Login Sign in form-->
+						<div class="login-signin">
+							<div class="mb-20">
+								<h3>Iniciar Sesion</h3>
+								<p class="opacity-60 font-weight-bold">Ingrese su usuario y contraseña</p>
+							</div>
+							<form class="form" id="kt_login_signin_form">
+								<div class="form-group">
+                                	<input id="email" type="email" class="form-control h-auto text-white placeholder-white opacity-70 bg-dark-o-70 rounded-pill border-0 py-4 px-8 mb-5 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                	@error('email')
+                                    	<span class="invalid-feedback" role="alert">
+                                        	<strong>{{ $message }}</strong>
+                                    	</span>
+                                	@enderror
+                            	</div>
+
+								<div class="form-group">
+									<input class="form-control h-auto text-white placeholder-white opacity-70 bg-dark-o-70 rounded-pill border-0 py-4 px-8 mb-5" type="password" placeholder="Password" name="password" />
+								</div>
+								<div class="form-group d-flex flex-wrap justify-content-between align-items-center px-8">
+									<label class="checkbox checkbox-outline checkbox-white text-white m-0">
+									<input type="checkbox" name="remember" />Remember me
+									<span></span></label>
+									<a href="javascript:;" id="kt_login_forgot" class="text-white font-weight-bold">Forget Password ?</a>
+								</div>
+								<div class="form-group text-center mt-10">
+									<button id="kt_login_signin_submit" class="btn btn-pill btn-outline-white font-weight-bold opacity-90 px-15 py-3">Sign In</button>
+								</div>
+							</form>
+							
+						</div>
+						<!--end::Login Sign in form-->
+						
+					</div>
+				</div>
+			</div>
+			<!--end::Login-->
+		</div>
+		<!--end::Main-->
+		<script>var HOST_URL = "https://keenthemes.com/metronic/tools/preview";</script>
+		<!--begin::Global Config(global config for global JS scripts)-->
+		<script>var KTAppSettings = { "breakpoints": { "sm": 576, "md": 768, "lg": 992, "xl": 1200, "xxl": 1200 }, "colors": { "theme": { "base": { "white": "#ffffff", "primary": "#3699FF", "secondary": "#E5EAEE", "success": "#1BC5BD", "info": "#8950FC", "warning": "#FFA800", "danger": "#F64E60", "light": "#F3F6F9", "dark": "#212121" }, "light": { "white": "#ffffff", "primary": "#E1F0FF", "secondary": "#ECF0F3", "success": "#C9F7F5", "info": "#EEE5FF", "warning": "#FFF4DE", "danger": "#FFE2E5", "light": "#F3F6F9", "dark": "#D6D6E0" }, "inverse": { "white": "#ffffff", "primary": "#ffffff", "secondary": "#212121", "success": "#ffffff", "info": "#ffffff", "warning": "#ffffff", "danger": "#ffffff", "light": "#464E5F", "dark": "#ffffff" } }, "gray": { "gray-100": "#F3F6F9", "gray-200": "#ECF0F3", "gray-300": "#E5EAEE", "gray-400": "#D6D6E0", "gray-500": "#B5B5C3", "gray-600": "#80808F", "gray-700": "#464E5F", "gray-800": "#1B283F", "gray-900": "#212121" } }, "font-family": "Poppins" };</script>
+		<!--end::Global Config-->
+		<!--begin::Global Theme Bundle(used by all pages)-->
+		<script src="{{ asset('metronic/dist/assets/plugins/global/plugins.bundle.js?v=7.0.4') }}"></script>
+		<script src="{{ asset('metronic/dist/assets/plugins/custom/prismjs/prismjs.bundle.js?v=7.0.4') }}"></script>
+		<script src="{{ asset('metronic/dist/assets/js/scripts.bundle.js?v=7.0.4') }}"></script>
+		<!--end::Global Theme Bundle-->
+		<!--begin::Page Scripts(used by this page)-->
+		<script src="{{ asset('metronic/dist/assets/js/pages/custom/login/login-general.js?v=7.0.4') }}"></script>
+		<!--end::Page Scripts-->
+	</body>
+	<!--end::Body-->
+</html>
